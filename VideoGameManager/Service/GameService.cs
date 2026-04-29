2d89f7fff328c5d634191048adde0ff90b5b3cbe
SebastianZadomen <sebastian.zadomen.7e8@itb.cat>
2026-04-29 18:43:39 +0200

    GameService class added, closes #4

---
+++ VideoGameManager/Service/GameService.cs
@@ -0,0 +1,60 @@
+﻿using System.Timers;
+using VideoGameManager.Models;
+
+namespace VideoGameManager.Service
+{
+    public class GameService
+    {
+        private  List<Game> Games { get; set; }
+
+        public GameService()
+        {
+            Games = new List<Game>();
+        }
+
+        public List<Game> GetAll()
+        {
+            return Games;
+        }
+        public Game GetById(int id)
+        {
+           Game searchId = Games.Find(x => x.Id == id);
+            return searchId;
+        }
+        public void Add(Game newGame)
+        {
+            Games.Add(newGame);
+        }
+        public void Update(Game newGame)
+        {
+            if (newGame != null)
+            {
+                foreach (var item in Games)
+                {
+                    if (item.Id == newGame.Id)
+                    {
+                        item.Title = newGame.Title;
+                        item.Genre = newGame.Genre;
+                        item.Year = newGame.Year;
+                        item.Score = newGame.Score;
+                        item.Description = newGame.Description;
+                        return;
+                    }
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("El elemento que tratas de actualizar no existe");
+            }
+        }
+        public void Delete(int id)
+        {
+            var gameDelete = GetById(id);
+            if (gameDelete != null)
+            {
+                Games.Remove(gameDelete);
+            }
+        }
+    }
+}
